f816b6abc6a1e49a56951034e2e678a9bd2645a2
Slawek <slapyt84@gmail.com>
2022-01-09 19:11:57 +0100

    Task 2: Uzupełniłem klasę CAR o brakujące metody

--- src/main/java/myPackage/exam/colections/Car.java
+++ src/main/java/myPackage/exam/colections/Car.java
@@ -2,13 +2,58 @@ package myPackage.exam.colections;
 
 import java.util.UUID;
 
-public class Car {
-    boolean isExclusive; //true or false
-    int yearOfProduction; // eg 2021, 2015 etc
-    int levelOfEquipment; // 3 > 2 > 1, higher then better
+public class Car implements Comparable<Car> {
+    Boolean isExclusive; //true or false
+    Integer yearOfProduction; // eg 2021, 2015 etc
+    Integer levelOfEquipment; // 3 > 2 > 1, higher then better
     UUID id;
-    double value; // how much does it cost for client
+    Double value; // how much does it cost for client
 
-    //todo dodaj wszystkie brakujące metody do klasy aby móc jej używać w zadaniu Task2
+    public Car(boolean isExclusive, int yearOfProduction, int levelOfEquipment, double value) {
+        this.isExclusive = isExclusive;
+        this.yearOfProduction = yearOfProduction;
+        this.levelOfEquipment = levelOfEquipment;
+        this.value = value;
+        this.id = UUID.randomUUID();
+    }
 
+    @Override
+    public String toString() {
+        return "Rok produkcji: " + yearOfProduction +
+                ", Poziom wyposażenia: " + levelOfEquipment +
+                ", Cena: " + value +
+                ", VIN: " + id +
+                ", Ekskluzywne: " + isExclusive;
+    }
+
+    public boolean isExclusive() {
+        return isExclusive;
+    }
+
+    public Integer getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public Integer getLevelOfEquipment() {
+        return levelOfEquipment;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        int result = o.isExclusive.compareTo(this.isExclusive);
+        if (result == 0) {
+            result = o.getYearOfProduction().compareTo(this.getYearOfProduction());
+            if (result == 0) {
+                result = o.getLevelOfEquipment().compareTo(this.getLevelOfEquipment());
+                if (result == 0) {
+                    result = o.getValue().compareTo(this.getValue());
+                }
+            }
+        }
+        return result;
+    }
 }
