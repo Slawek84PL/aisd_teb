3f9faba21657e4f767f4819c58724cea03816ffc
Slawek <slapyt84@gmail.com>
2022-01-11 18:28:04 +0100

    Task 2: Wypełniłem listę carList

--- src/main/java/myPackage/exam/colections/Task2.java
+++ src/main/java/myPackage/exam/colections/Task2.java
@@ -1,10 +1,28 @@
 package myPackage.exam.colections;
 
+import javax.management.ObjectInstance;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Task2 {
-    //przynajmniej 10 elementów, pokrywająca większość przypadków i umożliwiająca po wyświetlonych danych ocenić działanie programu
-    private static List<Car> carList; //todo ma być posortowana po roczniku, auta nowesze jako pierwsze a starsze później, i stale utzymywać porządek
+    static Car auto1 = new Car(true, 2021, 2, 180000);
+    static Car auto2 = new Car(false, 2021, 3, 200000);
+    static Car auto3 = new Car(false, 2021, 1, 160000);
+    static Car auto4 = new Car(true, 2020, 3, 170000);
+    static Car auto5 = new Car(false, 2020, 1, 110000);
+    static Car auto6 = new Car(false, 2020, 2, 130000);
+    static Car auto7 = new Car(true, 2019, 3, 115000);
+    static Car auto8 = new Car(true, 2019, 3, 125000);
+    static Car auto9 = new Car(false, 2019, 2, 100000);
+    static Car auto10 = new Car(false, 2019, 1, 90000);
+    static Car auto11 = new Car(false, 2021, 1, 120000);
+    static Car auto12 = new Car(true, 2020, 2, 150000);
+    static Car auto13 = new Car(false, 2018, 1, 70000);
+    static Car auto14 = new Car(false, 2019, 1, 80000);
+
+    private static List<Car> carList = new LinkedList<>(Arrays.asList(auto1, auto2, auto3, auto4, auto5, auto6, auto7, auto8, auto9, auto10, auto11));
+    //todo ma być posortowana po roczniku, auta nowesze jako pierwsze a starsze później, i stale utzymywać porządek
 
     public static List<Car> getCarList(){ //TIP singleton
         //zwraca aktualny stan listy aut
